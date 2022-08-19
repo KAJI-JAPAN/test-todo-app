3b27dba1216e6e1f6158b837762ecdfa41f88f67
KAJI-JAPAN <kurowassan1206@gmail.com>
2022-08-19 10:39:27 +0900

    タスク追加機能を追加

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,25 +1,52 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export const TodoList = () => {
   const initialState = [
     {
-      task: 'Learn vue.js',
-      isCompleted: false
+      task: "Learn vue.js",
+      isCompleted: false,
     },
     {
-      task: 'Learn React Hook',
-      isCompleted: false
+      task: "Learn React Hook",
+      isCompleted: false,
     },
     {
-      task: 'Learn Gatsby.js',
-      isCompleted: false
-    },     
-  ]
-  const [ todos, setTodos ] = useState(initialState)
+      task: "Learn Gatsby.js",
+      isCompleted: false,
+    },
+  ];
+  const [todos, setTodos] = useState(initialState);
+  const [task, setTask] = useState("");
+
+  const hundleNewTask = (e) => {
+    setTask(e.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (task === "") return;
+    console.log(...todos);
+    setTodos((todos) => [...todos, { task, isCompleted: false }]);
+    setTask("");
+  };
 
   return (
     <div>
-       <h1> Todo List </h1>
+      <h1> Todo List </h1>
+      <form onSubmit={handleSubmit}>
+        Add Task :
+        <input
+          placeholder="Add New Task"
+          value={task}
+          onChange={hundleNewTask}
+        />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}> {todo.task} </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
